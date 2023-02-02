597e3893f7f1cddfef6735080b504ee84fb7a0d7
AndreaTerron <37133810+AndreaTerron@users.noreply.github.com>
2023-02-02 08:41:55 +0100

    AÑADIMOS SERVO

--- main.ts
+++ main.ts
@@ -1,17 +1,29 @@
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "lumus") {
-        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 1)
     }
     if (receivedString == "off") {
-        pins.digitalWritePin(DigitalPin.P0, 0)
+        servos.P0.setAngle(0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+    if (receivedString == "ALOJOMORA") {
+        servos.P0.setAngle(160)
     }
 })
 radio.setGroup(1)
+pins.digitalWritePin(DigitalPin.P1, 0)
+servos.P0.setAngle(0)
 basic.forever(function () {
     if (input.isGesture(Gesture.TiltLeft)) {
         basic.pause(1000)
         if (input.isGesture(Gesture.LogoUp)) {
             radio.sendString("lumus")
+        }
+    }
+    if (input.isGesture(Gesture.LogoUp)) {
+        basic.pause(1000)
+        if (input.isGesture(Gesture.LogoDown)) {
+            radio.sendString("ALOJOMORA")
         }
     }
     if (input.isGesture(Gesture.Shake)) {
